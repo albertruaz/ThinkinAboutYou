709b52bf3389eeac60ea74a54b4b6542701130dc
albertruaz <albertruaz@gmail.com>
2024-11-13 01:08:40 +0900

    Add: Tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 파일 확장자에 맞게 설정
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
